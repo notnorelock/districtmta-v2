bd4c0320b8ce9e271f31b4bd8e72e13b05bbcf83
Norbert Podbiegly <78374441+jacuniadev@users.noreply.github.com>
2026-08-19 00:23:44 +0200

    Format code for consistency and readability

--- [custom]/models/client.lua
+++ [custom]/models/client.lua
@@ -1,21 +1,26 @@
 MODELS = {
-    { txd='vehicle/sapd/596.txd', dff='vehicle/sapd/596.dff', model = 596 },
-	{ txd='vehicle/sapd/598.txd', dff='vehicle/sapd/598.dff', model = 598 },
-	{ txd='vehicle/sapd/599.txd', dff='vehicle/sapd/599.dff', model = 599 },
+    { txd = "vehicle/sapd/596.txd", dff = "vehicle/sapd/596.dff", model = 596 },
+	{ txd = "vehicle/sapd/598.txd", dff = "vehicle/sapd/598.dff", model = 598 },
+	{ txd = "vehicle/sapd/599.txd", dff = "vehicle/sapd/599.dff", model = 599 }
 }
 
-addEventHandler('onClientResourceStart', resourceRoot, function()
-	if MODELS then
-		for _, v in ipairs(MODELS) do
-			if v.txd then
-				local txd = engineLoadTXD(v.txd)
-				engineImportTXD(txd, model or v.model)
-			end
-				
-			if v.dff then
-				local dff = engineLoadDFF(v.dff)
-				engineReplaceModel(dff, model or v.model)
-			end
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	for _, v in ipairs(MODELS) do
+		if v.txd then
+			local txd = engineLoadTXD(v.txd)
+			engineImportTXD(txd, model or v.model)
 		end
+				
+		if v.dff then
+			local dff = engineLoadDFF(v.dff)
+			engineReplaceModel(dff, model or v.model)
+		end
+	end
+end)
+
+addEventHandler("onClientResourceStop", resourceRoot, function()
+	for _, v in pairs(MODELS) do
+		engineRestoreModel(v.model)
+		engineRestoreCOL(v.model)
 	end
 end)
