bec636a569ca63ac4e748c422235d91f09e1a85b
Norbert Podbiegly <78374441+jacuniadev@users.noreply.github.com>
2026-08-20 22:04:11 +0200

    remove Character model and repository for cleanup

--- [core]/core/server/database/repositories/CharacterRepository.lua
+++
@@ -1,7 +0,0 @@
-CharacterRepository = CharacterRepository or {}
-
---- @param accountId string
--- @param callback function(ok: boolean, charactersOrError: table|string)
-CharacterRepository.findByAccountId = function(accountId, callback)
-    Character:where("account_id", accountId):orderBy("created_at", "ASC"):get(callback)
-end
